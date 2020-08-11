a5492c8d04060adfff82c09f70c66baacc0fbac9
Patrick Duque <patpatduque26@gmail.com>
2020-08-11 16:03:58 +0800

    Updates

---
+++ precache-manifest.53e1dd4540bf240fd0f334f9788edc91.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da056581102bde22fc7cbf92fb71ff08",
+    "url": "/BurgerBuilder/index.html"
+  },
+  {
+    "revision": "c7366350b721e3892791",
+    "url": "/BurgerBuilder/static/css/main.7afd9fae.chunk.css"
+  },
+  {
+    "revision": "d0e5664e89e6c9721499",
+    "url": "/BurgerBuilder/static/js/2.7a7d88df.chunk.js"
+  },
+  {
+    "revision": "a7a190ca9edf4019692023d347b5104f",
+    "url": "/BurgerBuilder/static/js/2.7a7d88df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f81a247789ee505c36c",
+    "url": "/BurgerBuilder/static/js/3.36243b55.chunk.js"
+  },
+  {
+    "revision": "062417752ee2d773712a",
+    "url": "/BurgerBuilder/static/js/4.06e30bf6.chunk.js"
+  },
+  {
+    "revision": "c7366350b721e3892791",
+    "url": "/BurgerBuilder/static/js/main.11c16234.chunk.js"
+  },
+  {
+    "revision": "7ad550576af9c0062088",
+    "url": "/BurgerBuilder/static/js/runtime-main.12063f6b.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/BurgerBuilder/static/media/burger-logo.b8503d26.png"
+  }
+]);
